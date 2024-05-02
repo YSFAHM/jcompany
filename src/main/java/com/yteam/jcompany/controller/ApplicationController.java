ba78e2e538130d68d6282e8e1461bc5d126dba0e
Youssef A <ysfahm1@outlook.com>
2024-05-02 14:14:06 +0300

    nothing

--- src/main/java/com/yteam/jcompany/controller/ApplicationController.java
+++ src/main/java/com/yteam/jcompany/controller/ApplicationController.java
@@ -1,9 +1,8 @@
 package com.yteam.jcompany.controller;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/applications")
-public class ApplicationController {    
+public class ApplicationController {
 }
